afa4564356bddd9f27d84abb8867d42588865c21
ahmed zakaria <Ahmed.aligokhy@gmail.com>
2017-12-03 16:43:49 +0200

    working progress

---
+++ binary_search.rb
@@ -0,0 +1,12 @@
+def binary_search(array, min, max, target)
+  return 'not found' if max > min
+  mid = min + ((max - min) / 2)
+  return mid if array[mid] == target
+  if target < array[mid]
+    binary_search(array, min, mid - 1, target)
+  elsif target > array[mid]
+    binary_search(array, mid + 1, max, target)
+  end
+end
+
+puts binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 9, -1)
